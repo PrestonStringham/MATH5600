364d82ce065a97bb22459777cab6af417de9edc2
Unknown <stringhampreston@gmail.com>
2019-01-12 03:43:34 -0700

    Completed Question 4 Part 2

---
+++ Homework 1/hw01q4.m
@@ -0,0 +1,14 @@
+x=[]
+y=[]
+for i = 0:15
+   x(i+1)=i
+   y(i+1)=nonsense(i)
+end
+
+figure
+plot(x,y,'--^')
+title('x vs. nonsense')
+xlabel('x') 
+ylabel('nonsense') 
+h = legend('point')
+set(h)
