fa00be58b78678c06610e296b02cd54340b67bcf
Unknown <stringhampreston@gmail.com>
2019-01-12 03:43:09 -0700

    Question 4 Part 1 Completed

---
+++ Homework 1/nonsense.m
@@ -0,0 +1,10 @@
+function y = nonsense(x)
+    if(mod(x,2)==1)
+        y = 0;
+    else
+        y = x + 1;
+    end 
+end
+% [4,11,15,20] put into printstuff outputs corresponding values
+% [17,122,226,401] with function as given in problem 3. Plugging into
+% nonsense, we get [0, 123, 127, 0], as expected.
