978c7f11a8d1e8de08ebbb2b7a29560799a7a1e4
Unknown <stringhampreston@gmail.com>
2019-01-18 11:35:10 -0700

    P Vector Complete and Output Vector Created

--- Homework 2/tobinary.m
+++ Homework 2/tobinary.m
@@ -1,4 +1,16 @@
 %Preston Stringham and River Griffin
 function tobinary(n)
-    
+    if (mod(n,1) ~= 0)
+        error('Number is not a whole number.');
+    else
+        pmax = floor(log2(n));
+        p = [pmax]
+        i=2
+        while pmax > 0
+           pmax = pmax-1;
+           p(i) = pmax
+           i = i + 1;
+        end
+        output = zeros(1,length(p))
+    end
 end
