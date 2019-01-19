a0ef313eb54282f5239d6c8301c71b5325aa7d83
Unknown <stringhampreston@gmail.com>
2019-01-18 17:37:12 -0700

    Completed Problem 4

--- Homework 2/tobinary.m
+++ Homework 2/tobinary.m
@@ -1,16 +1,21 @@
 %Preston Stringham and River Griffin
-function tobinary(n)
+function [vector] = tobinary(n)
     if (mod(n,1) ~= 0)
         error('Number is not a whole number.');
     else
-        pmax = floor(log2(n));
-        p = [pmax]
-        i=2
-        while pmax > 0
-           pmax = pmax-1;
-           p(i) = pmax
-           i = i + 1;
+        dummy = zeros;
+        i = 1;
+        while n > 0
+            pmax = floor(log2(n)); %get index of log2
+            dummy(i) = pmax %store the index
+            n = n - (2.^pmax); %update number by subtracting 2^pmax value
+            i = i + 1; %increment index for dummy vector
         end
-        output = zeros(1,length(p))
+        maxi = max(dummy) + 1;
+        vector = zeros(1, maxi);
+        for j = 1:length(dummy)
+            vector(dummy(j)+1) = 1;
+        end
+        vector = fliplr(vector);
     end
 end
