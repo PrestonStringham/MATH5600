7b5c479438fc654e0dc0ec4ed11957def0b1eff1
River <griffinjade8@gmail.com>
2019-01-20 09:58:38 -0700

    Removed if isempty() statement. We dont need it

--- Homework 2/tobinary.m
+++ Homework 2/tobinary.m
@@ -9,12 +9,7 @@ function [vector] = tobinary(n)
         end
         while n > 0
             pmax = floor(log2(n)); %get index of log2
-            if isempty(vector) %if vector is empty fill it with its max
-                vector = zeros(1, pmax+1);
-                vector(pmax+1)=1;
-            else
-                vector(pmax+1)=1;
-            end
+            vector(pmax+1)=1; %add 1 to the index of the vector
             n = n - (2.^pmax); %update number by subtracting 2^pmax value
         end
         vector = fliplr(vector);
