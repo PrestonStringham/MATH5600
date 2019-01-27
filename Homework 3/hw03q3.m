809a12574f46d43fef11b11a44cea49506a88956
River <griffinjade8@gmail.com>
2019-01-27 16:51:38 -0700

    Updated homework 3

--- Homework 3/hw03q3.m
+++ Homework 3/hw03q3.m
@@ -1,5 +1,17 @@
-k=15;
-A=full(delsq(numgrid('B', k)));
-b=zeros(k,1)+1
-[L,U] = MakeLU(A)
-x=ForwardSubstitution(A,b)
+%Preston Stringham & River Griffin
+for k=5:7
+    k.^2 %display number of elements
+    A=full(delsq(numgrid('B', k.^2)));
+    b=zeros(length(A),1)+1;
+    startdecom=tic; %start timer for decomposition
+    [L,U] = MakeLU(A);
+    toc(startdecom) %show time to decompose matrix A
+    startsolve = tic; %start timer for solving
+    y=ForwardSubstitution(L,b); %solve for y
+    x=BackSubstitution(U,y); %solve for x
+    toc(startsolve) %show time to solve
+    r = A*x-b;
+    disp('Norm: ')
+    norm(r) %display norm
+end
+
