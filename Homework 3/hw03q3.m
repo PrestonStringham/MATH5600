4d678edbb32c3adf274fe20c2f744fda3d279eab
River <griffinjade8@gmail.com>
2019-01-28 13:11:23 -0700

    Added fprintf statement to display information

--- Homework 3/hw03q3.m
+++ Homework 3/hw03q3.m
@@ -1,17 +1,17 @@
 %Preston Stringham & River Griffin
 for k=5:7
-    k.^2 %display number of elements
     A=full(delsq(numgrid('B', k.^2)));
     b=zeros(length(A),1)+1;
     startdecom=tic; %start timer for decomposition
     [L,U] = MakeLU(A);
-    toc(startdecom) %show time to decompose matrix A
+    decompose=toc(startdecom); %grab time to solve
     startsolve = tic; %start timer for solving
     y=ForwardSubstitution(L,b); %solve for y
     x=BackSubstitution(U,y); %solve for x
-    toc(startsolve) %show time to solve
+    solve=toc(startsolve); %grab time to solve
     r = A*x-b;
-    disp('Norm: ')
-    norm(r) %display norm
+    resid=norm(r); %display norm
+    formatspec='Number of elments: %d \nDecompose Time: %f \nSolve Time: %f \nNorm: %s \n\n';
+    fprintf(formatspec, k.^2, decompose, solve, resid); %display info
 end
 
