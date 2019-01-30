a21f01f9519ac4edb67c737d632b2321a068c5f2
Unknown <stringhampreston@gmail.com>
2019-01-29 17:32:14 -0700

    Potential solution to question 4 for HW 3

--- Homework 3/MakePLU.m
+++ Homework 3/MakePLU.m
@@ -1,14 +1,21 @@
 function [L,U,P]=MakePLU(A)
     n = size(A,1);
     L=eye(n);
-
+    P=eye(n);
+    
     for k=1:n-1
-        % Check to see if the pivot is zero
-        if abs(A(k,k)) < 1e-15
-            
-        end
-
         for i=k+1:n
+     
+            col = A(:,k); %Get the kth column of A
+            [pivot, index] = max(col); %Get the largest element in the column
+            dummyU = A(k,:); %Temp variable to store kth row for U
+            dummyP = P(k,:); %Temp variable to store kth row for P
+            A(k,:) = A(index,:); %Replace kth row in U with row that has greatest value
+            P(k,:) = P(index,:); %Replace kth row in P with row that has greates value in U
+            A(index,:) = dummyU; %Replace row that had greatest value with kth row
+            P(index,:) = dummyP; %Replace row that had greatest value with kth row in P
+            
+            %Finish LU Decomp
             m = A(i,k)/A(k,k); %multiplier for current row i
             L(i,k) = m;
             for j=k:n
