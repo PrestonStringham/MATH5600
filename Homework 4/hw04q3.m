0956951c0ba9926fd6a435a296684247fbf20493
Unknown <stringhampreston@gmail.com>
2019-02-01 11:29:03 -0700

    Completed Q4 except explanation

---
+++ Homework 4/hw04q3.m
@@ -0,0 +1,11 @@
+A = [3 4; -3 -6.001]
+b = [2; 3]
+x = A\b
+b1=[2;3.01]
+x1 = A\b
+relx=norm(x-x1)/norm(x)
+relb=norm(b-b1)/norm(b)
+condA=cond(A)
+relx <= condA*relb
+
+%Yes, this agrees with the Theorem in problem 2. 
