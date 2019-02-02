c52510429ced751cfff7358c5b82b81e031e9110
River <griffinjade8@gmail.com>
2019-02-02 15:07:49 -0700

    added comment

--- Homework 4/hw04q4.m
+++ Homework 4/hw04q4.m
@@ -1,17 +1,19 @@
 M = [10^-14 1 0; 1 0 0; 0 1 1]
 b = [2; 1; 5]
 
-x_ref=M/b'; %Not sure what x_ref is supposed to be. Which is the analytical solution?
+%Solve using MATLAB 'backslash'
+x=M/b'; %Not sure what x_ref is supposed to be. Which is the analytical solution?
 
 %Solve with MakeLU
 [L,U] = MakeLU(M);
 y = L/b';
 x = U/y';
 
-norm(x - xref)
 
 %Solve with MakePLU. P^-1*M=P^-1*b
 [L1,U1,P1] = MakePLU(M);
+
+
 
 
 
