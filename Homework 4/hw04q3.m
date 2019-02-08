dc12dcd501913a27db694165faeb55c02944a78b
Preston <stringhampreston@gmail.com>
2019-02-07 21:19:35 -0700

    Fixed

--- Homework 4/hw04q3.m
+++ Homework 4/hw04q3.m
@@ -4,9 +4,9 @@ b = [2; 3]
 x = A\b
 b1=[2;3.01]
 x1 = A\b1
-relx=norm(x-x1)/norm(x)
-relb=norm(b-b1)/norm(b)
-condA=cond(A)
+relx=norm(x-x1, inf)/norm(x, inf)
+relb=norm(b-b1, inf)/norm(b, inf)
+condA=cond(A, inf)
 relx <= condA*relb
 
 %Yes, this agrees with the Theorem in problem 2. See paper for details.
