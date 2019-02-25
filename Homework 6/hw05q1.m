c9e3bfdda2168044e253cb0708a04bfd99855d56
Unknown <stringhampreston@gmail.com>
2019-02-25 10:46:09 -0700

    Initial Commit HW 6

---
+++ Homework 6/hw05q1.m
@@ -0,0 +1,27 @@
+%Preston Stringham and River Griffin
+
+fprintf('----------------------------------------- \n')
+fprintf('x_0 = 0 \n')
+format short
+h=2.^-(1:10);
+x0=0;
+f= @(x)sin(x);
+fd = (f(x0+h)-f(x0)) ./ h;
+exact = cos(x0);
+error = abs(fd-exact);
+ratio = [0 error(1:end-1) ./ error(2:end)];
+[h' fd' error' ratio']
+fprintf('----------------------------------------- \n')
+
+fprintf('----------------------------------------- \n')
+fprintf('x_0 = 1 \n')
+format short
+h=2.^-(1:10);
+x0=1;
+f= @(x)sin(x);
+fd = (f(x0+h)-f(x0)) ./ h;
+exact = cos(x0);
+error = abs(fd-exact);
+ratio = [0 error(1:end-1)./error(2:end)];
+[h' fd' error' ratio']
+fprintf('----------------------------------------- \n')
