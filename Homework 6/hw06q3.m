3f76d87561d267bd25c592f872f5f3f94b4b64e0
Unknown <stringhampreston@gmail.com>
2019-02-27 11:36:22 -0700

    Finished Question 3

--- Homework 6/hw06q3.m
+++ Homework 6/hw06q3.m
@@ -10,28 +10,26 @@ b = 9;
 ua = -1;
 ub = 5;
 
+%N=6 Values
+N_max = 15;
+j = 1;
+for(i = 2.^(0:N_max)*6)
+    [x,u] = finite_difference_laplace(f, a, b, i, ua, ub);
+    h(j) = x(2)-x(1);
+    error(j) = max(abs(f_exact(x) - u'));
+    j=j+1;
+    if(i == 6 || i == 12 || i == 24)
+       plot(x,u,'x-');
+       hold on
+    end
+end
+
+rate = [0 error(1:end-1) ./ error(2:end)];
+
 %Exact Solution Plot
 xx=linspace(a,b,1000);
 yexact=f_exact(xx);
 plot(xx,yexact, 'k-')
-hold on
-
-%N=6 Values
-N = 6;
-[x1,u1] = finite_difference_laplace(f, a, b, N, ua, ub);
-plot(x1,u1,'x-')
-hold on
-
-%N=12 Values
-N=12;
-[x2,u2] = finite_difference_laplace(f, a, b, N, ua, ub);
-plot(x2,u2,'x-')
-hold on
-
-%N=24 Values
-N=24;
-[x3,u3] = finite_difference_laplace(f, a, b, N, ua, ub);
-plot(x3,u3,'x-')
 hold on
 
 %Set Plot Title, Legend, and LineWidth.
@@ -39,20 +37,9 @@ title("BVP Approximations")
 legend("Exact Solution", "N=6", "N=12", "N=24", 'Location', 'northwest');
 set(findall(gca, 'Type', 'Line'),'LineWidth',1.5);
 
-%Compute h Values
-h1 = x1(2)-x1(1);
-h2 = x2(2)-x2(1);
-h3 = x3(2)-x3(1);
-h = [h1 h2 h3];
-
-%Compute Errors
-error1 = max(abs(f_exact(x1) - u1'));
-error2 = max(abs(f_exact(x2) - u2'));
-error3 = max(abs(f_exact(x3) - u3'));
-error = [error1 error2 error3];
-
-%Compute Convergence Rate
-rate = [0 error(1:end-1) ./ error(2:end)];
-
+fprintf('----------------------------------------- \n')
 [h' error' rate']
+fprintf('The method converges of order O(h^2). \n')
+
+fprintf('----------------------------------------- \n')
 
