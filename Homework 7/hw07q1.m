74c2a0cf76bfa17ec1fa919049f026b1ff0587f8
Unknown <stringhampreston@gmail.com>
2019-03-04 12:17:43 -0700

    Finished Question 1?

---
+++ Homework 7/hw07q1.m
@@ -0,0 +1,23 @@
+%Preston Stringham and River Griffin
+
+%Function
+f = @(x) sin(x) + 5*x.^2 - exp(x);
+
+%Tolerance
+tol = 1e-6;
+
+%First guess, take the entire interval we know the roots are on. We are
+%gauranteed to find a root.
+a=-5;
+b=5;
+root1=bisect(a,b,f,tol)
+
+%From the first guess, change a and b to begin finding second root.
+a=0;
+b=1;
+root2=bisect(a,b,f,tol)
+
+%From the second guess, change a and b to begin finding the third root.
+a=1;
+b=5;
+root3=bisect(a,b,f,tol)
