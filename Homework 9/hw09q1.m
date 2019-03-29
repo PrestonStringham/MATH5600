0a3b2b94a7b88d0502abd071ac0c71174a173119
Preston <stringhampreston@gmail.com>
2019-03-29 16:16:55 -0600

    Finished Question 1?

--- Homework 9/hw09q1.m
+++ Homework 9/hw09q1.m
@@ -1,8 +1,8 @@
 %Preston Stringham and River Griffin
-
+clc; clear;
 %Part (a)
-px = [0 2 3 4 5 6 7]
-py = [-1 -2 -1 3 -1 -1 5]
+px = [0 2 3 4 5 6 7];
+py = [-1 -2 -1 3 -1 -1 5];
 
 n = length(px);
 
@@ -11,7 +11,7 @@ for i=1:n
     A(:,i)=px.^(i-1);
 end
 c=A\py';
-c
+c;
 
 %Monomial interpolation
 pn= @(x) c(1) + c(2)*x + c(3)*x.^2 + c(4)*x.^3 + c(5)*x.^4 + c(6)*x.^5 ...
@@ -32,25 +32,40 @@ plot(xx, yy, '-')
 hold on
 
 %Piecewise linear interpolation
-plot(px, py, '-', px, py, 'x')
+plot(px, py, '-')
 hold on
 
 
 %Part (b)
 
 %Condition number
-num = cond(A, inf)
+cond_num = cond(A, inf)
 
 %Part (c)
 
 %Monomial interpolation
-pn(1)
+monomial_at_1 = pn(1)
 
-ppval(PP, 1)
+spline_at_1 = ppval(PP, 1)
 
-m = (py(2)-py(1))/(px(2)-px(1))
+m = (py(2)-py(1))/(px(2)-px(1));
+y = @(x) m*x - 1;
+xx = linspace(0,2);
+yy = y(xx);
 
-lin = m*1 - 1
+linear_at_1 = y(1)
 
-legend("Monomial", "Spline", "Piecewise Linear", "Actual Linear", "Points")
+plot(xx, yy, '-', px, py, 'x')
+
+legend("Monomial", "Spline", "Fake Piecewise Linear",...
+    "Actual Piecewise Linear", "Points");
+
 %Part (d)
+x0 = 1;
+h = 1e-3;
+
+cd_monomial = (pn(x0+h)-pn(x0-h)) ./ (2.*h)
+
+cd_spline = (ppval(PP, (x0+h))-(ppval(PP, (x0-h)))) ./ (2.*h)
+
+cd_linear = (y(x0+h)-y(x0-h)) ./ (2.*h)
