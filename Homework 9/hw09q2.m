7da0c1cbe32817f9a25cf85b0a782632a3f132e9
River <griffinjade8@gmail.com>
2019-04-02 18:37:34 -0600

    Rearranged code

--- Homework 9/hw09q2.m
+++ Homework 9/hw09q2.m
@@ -17,15 +17,14 @@ TRF1 = ((x(2)-x(1))/2)*(f1(x(1)) + f1(x(2)))
 TRF2 = ((x(2)-x(1))/2)*(f2(x(1)) + f2(x(2)))
 
 %Actual values for the functions integrated using MATLAB integral function
-
 q1 = integral(f1,x(1),x(2))
 q2 = integral(f2,x(1),x(2))
 
 
-%error for each calculated value of midpoint and trapazoid
-
+%error for each calculated value of midpoint and trapazoid to the acutal
 errorMIDF1 = abs(q1 - MIDF1)
-errorMIDF2 = abs(q2 - MIDF2)
-
 errorTRF1 = abs(q1 - TRF1)
+
+errorMIDF2 = abs(q2 - MIDF2)
 errorTRF2 = abs(q2 - TRF2)
+
