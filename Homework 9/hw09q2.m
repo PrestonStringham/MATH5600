768c660f5fe03bb4c7f0a2fc9bbd4831aa64c3ae
River <griffinjade8@gmail.com>
2019-04-04 08:38:32 -0600

    Final commit for hw 9.

--- Homework 9/hw09q2.m
+++ Homework 9/hw09q2.m
@@ -8,17 +8,17 @@ f1 = @(x) (sin(x)).^2;
 f2 = @(x) cos(x); 
 
 %Midpoint Rule
-MIDF1 = (x(2)-x(1))*f1(((x(2)+x(1))/2))
-MIDF2 = (x(2)-x(1))*f2(((x(2)+x(1))/2))
+MIDF1 = (x(2)-x(1))*f1(((x(2)+x(1))/2));
+MIDF2 = (x(2)-x(1))*f2(((x(2)+x(1))/2));
 
 
 %Trapezoid Rule
-TRF1 = ((x(2)-x(1))/2)*(f1(x(1)) + f1(x(2)))
-TRF2 = ((x(2)-x(1))/2)*(f2(x(1)) + f2(x(2)))
+TRF1 = ((x(2)-x(1))/2)*(f1(x(1)) + f1(x(2)));
+TRF2 = ((x(2)-x(1))/2)*(f2(x(1)) + f2(x(2)));
 
 %Actual values for the functions integrated using MATLAB integral function
-q1 = integral(f1,x(1),x(2))
-q2 = integral(f2,x(1),x(2))
+q1 = integral(f1,x(1),x(2));
+q2 = integral(f2,x(1),x(2));
 
 
 %error for each calculated value of midpoint and trapazoid to the acutal
